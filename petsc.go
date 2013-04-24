735186c92b11fd0699e3333fa87092fb8e55f6e2
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-04-23 21:01:48 -0400

    Initial commits....

---
+++ petsc.go
@@ -0,0 +1,9 @@
+// Package PETSc contains bindings between PETSc and Go. 
+
+/* 
+Author : Nikhil Padmanabhan, Yale Univ.
+
+This uses pkg-config to handle all the setup.
+An example file is in the pkgconfig directory.
+
+*/
