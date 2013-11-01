3a1d13fe228e30f642a9f4793b95471fcd9fa607
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-11-01 16:19:32 -0400

    Lots of updates

--- exec/qpm/qpm-rdzw2xyzwi/main.go
+++ exec/qpm/qpm-rdzw2xyzwi/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"math"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/npadmana/npgo/cosmo"
 	"github.com/npadmana/npgo/gnuplot"
@@ -15,6 +17,27 @@ import (
 )
 
 type Pos [3]float64
+
+var (
+	loPos = Pos{-math.MaxFloat64, -math.MaxFloat64, -math.MaxFloat64}
+	hiPos = Pos{math.MaxFloat64, math.MaxFloat64, math.MaxFloat64}
+)
+
+func (p *Pos) Min(p1 Pos) {
+	for i := range p {
+		if p1[i] < p[i] {
+			p[i] = p1[i]
+		}
+	}
+}
+
+func (p *Pos) Max(p1 Pos) {
+	for i := range p {
+		if p1[i] > p[i] {
+			p[i] = p1[i]
+		}
+	}
+}
 
 // The weight file is assumed to have a first header line, and then two columns
 // with z and n(z)
@@ -106,7 +129,7 @@ func splineCosmo(om, zmin, zmax float64) (*spline.Spline, error) {
 	return sp, nil
 }
 
-func doOne(infn, outfn string, zmin, zmax float64, dist, fkp *spline.Spline) error {
+func doOne(infn, outfn string, zmin, zmax float64, dist, fkp *spline.Spline, minpos, maxpos *Pos) error {
 	var err error
 	var ra, dec, zred, r, w, theta, phi float64
 	var ncol, ind int
@@ -125,26 +148,36 @@ func doOne(infn, outfn string, zmin, zmax float64, dist, fkp *spline.Spline) err
 	defer gg.Close()
 
 	ind = 1
+	elim := 0
+	maxz := -1.0
+	minz := 10.0
 	for err == nil {
 		ncol, err = fmt.Fscanln(ff, &ra, &dec, &zred, &w)
 		if err != nil {
 			continue
 		}
 		if ncol != 4 {
-			return errors.New("Error, incorrect number of columns read in")
+			return errors.New("Error, incorrect number of columns read in " + infn)
+		}
+		if zred < minz {
+			minz = zred
+		}
+		if zred > maxz {
+			maxz = zred
 		}
 		if (zred < zmin) || (zred >= zmax) {
+			elim++
 			continue
 		}
 		theta = (math.Pi / 180) * (90 - dec)
 		phi = (math.Pi / 180) * ra
 		w, err = fkp.Eval(zred)
 		if err != nil {
-			panic("Error in fkp spline")
+			panic("Error in fkp spline in " + infn)
 		}
 		r, err = dist.Eval(zred)
 		if err != nil {
-			panic("Error in dist spline")
+			panic("Error in dist spline " + infn)
 		}
 		p[0] = r * math.Sin(theta) * math.Cos(phi)
 		p[1] = r * math.Sin(theta) * math.Sin(phi)
@@ -153,14 +186,19 @@ func doOne(infn, outfn string, zmin, zmax float64, dist, fkp *spline.Spline) err
 		if err != nil {
 			panic("Error while writing file")
 		}
+		minpos.Min(p)
+		maxpos.Max(p)
 		ind++
 	}
 
+	fmt.Printf("%s had zmin, zmax = %f, %f, %d objects removed \n", infn, minz, maxz, elim)
 	return nil
 
 }
 
 func main() {
+	tstart := time.Now()
+
 	var wfn, infmt, outfmt string
 	var help bool
 	var Pk, zmin, zmax, om float64
@@ -206,14 +244,37 @@ func main() {
 	defer dist.Free()
 
 	var infn, outfn string
+	minpos := hiPos
+	maxpos := loPos
+	var lock sync.Mutex
+	var wg sync.WaitGroup
+
 	for ii := nstart; ii < nend; ii++ {
-		infn = fmt.Sprintf(infmt, ii)
-		outfn = fmt.Sprintf(outfmt, ii)
-		fmt.Printf("Processing %s --> %s ...\n", infn, outfn)
-		err = doOne(infn, outfn, zmin, zmax, dist, fkp)
-		if err != nil {
-			log.Fatal(err)
-		}
+		wg.Add(1)
+		go func(ii int) {
+			myminpos := hiPos
+			mymaxpos := loPos
+			infn = fmt.Sprintf(infmt, ii)
+			outfn = fmt.Sprintf(outfmt, ii)
+			fmt.Printf("Processing %s --> %s ...\n", infn, outfn)
+			err = doOne(infn, outfn, zmin, zmax, dist, fkp, &myminpos, &mymaxpos)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("My min:", myminpos)
+			fmt.Println("My max:", mymaxpos)
+			lock.Lock()
+			minpos.Min(myminpos)
+			maxpos.Max(mymaxpos)
+			lock.Unlock()
+			wg.Done()
+		}(ii)
 	}
+
+	wg.Wait()
+	fmt.Println("The minimum particle position was at ", minpos)
+	fmt.Println("The maximum particle position was at ", maxpos)
+
+	fmt.Println("The total elapsed time was ", time.Since(tstart))
 
 }
