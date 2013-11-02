e2804f7e71695c22b30a7fd04601a5f075ede065
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-11-02 02:16:07 -0400

    Catch subtle race condition

--- exec/qpm/qpm-rdzw2xyzwi/main.go
+++ exec/qpm/qpm-rdzw2xyzwi/main.go
@@ -250,7 +250,6 @@ func main() {
 	}
 	defer dist.Free()
 
-	var infn, outfn string
 	minpos := hiPos
 	maxpos := loPos
 	var lock sync.Mutex
@@ -258,11 +257,11 @@ func main() {
 
 	for ii := nstart; ii < nend; ii++ {
 		wg.Add(1)
-		go func(ii int) {
+		go func(ifn int) {
 			myminpos := hiPos
 			mymaxpos := loPos
-			infn = fmt.Sprintf(infmt, ii)
-			outfn = fmt.Sprintf(outfmt, ii)
+			infn := fmt.Sprintf(infmt, ii)
+			outfn := fmt.Sprintf(outfmt, ii)
 			fmt.Printf("Processing %s --> %s ...\n", infn, outfn)
 			err = doOne(infn, outfn, zmin, zmax, dist, fkp, &myminpos, &mymaxpos)
 			if err != nil {
@@ -275,7 +274,7 @@ func main() {
 			maxpos.Max(mymaxpos)
 			lock.Unlock()
 			wg.Done()
-		}(ii)
+		}(ifn)
 	}
 
 	wg.Wait()
