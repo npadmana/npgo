7d318fa383e0a2747d4805ee7d121d6f49ff06ee
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-11-06 14:47:16 -0500

    more stuff

--- setups/setups.edison.csh
+++ setups/setups.edison.csh
@@ -1,8 +1,9 @@
 setenv NPGO_DIR /global/homes/n/npadmana/myWork/gocode/src/github.com/npadmana/npgo
+setenv MPI_DIR $MPICH_DIR
 
 #----- We should try to ensure that everything below is automatic
-setenv PKG_CONFIG_PATH ${NPGO_DIR}/local/lib/pkgconfig:${PKG_CONFIG_PATH}:${NPGO_DIR}/pkgconfigs
+setenv PKG_CONFIG_PATH ${NPGO_DIR}/local/lib/pkgconfig:${MPI_DIR}/lib:${NPGO_DIR}/pkgconfigs
 setenv LD_LIBRARY_PATH ${NPGO_DIR}/local/lib:${NPGO_DIR}/clibs:${LD_LIBRARY_PATH}
 export PETSC_DIR=$NPGO_DIR/local
-export C_INCLUDE_PATH=$NPGO_DIR/clibs:$C_INCLUDE_PATH
+export C_INCLUDE_PATH=$NPGO_DIR/clibs
 
--- setups/setups.laptop.sh
+++ setups/setups.laptop.sh
@@ -1,9 +1,10 @@
 export NPGO_DIR=/Users/npadmana/myWork/gocode/src/github.com/npadmana/npgo
+export MPI_DIR=/usr/local/openmpi/v1.6-gcc47/
 
 #----- We should try to ensure that everything below is automatic
-export PKG_CONFIG_PATH=$NPGO_DIR/local/lib/pkgconfig:$PKG_CONFIG_PATH:$NPGO_DIR/pkgconfigs
+export PKG_CONFIG_PATH=$NPGO_DIR/local/lib/pkgconfig:$MPI_DIR/lib:$NPGO_DIR/pkgconfigs
 export PETSC_DIR=$NPGO_DIR/local
-export C_INCLUDE_PATH=$NPGO_DIR/clibs:$C_INCLUDE_PATH
+export C_INCLUDE_PATH=$NPGO_DIR/clibs:
 export DYLD_LIBRARY_PATH=$NPGO_DIR/clibs:$DYLD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$NPGO_DIR/clibs:$LD_LIBRARY_PATH
 
