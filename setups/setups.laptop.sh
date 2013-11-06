0dc2c07d92fda369483cd8ca43da1b77ac49e0f0
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-11-06 13:46:11 -0500

    Minor updates

--- setups/setups.laptop.sh
+++ setups/setups.laptop.sh
@@ -2,3 +2,4 @@ export NPGO_DIR=/Users/npadmana/myWork/gocode/src/github.com/npadmana/npgo
 
 #----- We should try to ensure that everything below is automatic
 export PKG_CONFIG_PATH=$NPGO_DIR/local/lib/pkgconfig:$PKG_CONFIG_PATH
+export PETSC_DIR=$NPGO_DIR/local
