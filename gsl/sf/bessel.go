2bbf07358dff33d78188cde6aa1af8694866eaf2
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-11-14 19:14:34 -0500

    Added in spherical bessel functions and specs for it

--- gsl/sf/bessel.go
+++ gsl/sf/bessel.go
@@ -12,7 +12,7 @@ import (
 	"github.com/npadmana/npgo/gsl"
 )
 
-// BesselJ retures Jn(x)
+// BesselJ returns Jn(x)
 func BesselJ(n int, x float64) float64 {
 	var y C.double
 	switch n {
@@ -30,6 +30,35 @@ func BesselJ(n int, x float64) float64 {
 func BesselJArr(nmin, nmax int, x float64) []float64 {
 	arr := make([]float64, nmax-nmin+1)
 	ret := C.gsl_sf_bessel_Jn_array(C.int(nmin), C.int(nmax), C.double(x), (*C.double)(&arr[0]))
+	if ret != 0 {
+		panic(gsl.Errno(ret))
+	}
+	return arr
+}
+
+// SphBessel returns the spherical bessel function jl(x)
+func SphBessel(l int, x float64) float64 {
+	var y C.double
+	switch l {
+	case 0:
+		y = C.gsl_sf_bessel_j0(C.double(x))
+	case 1:
+		y = C.gsl_sf_bessel_j1(C.double(x))
+	case 2:
+		y = C.gsl_sf_bessel_j2(C.double(x))
+	default:
+		y = C.gsl_sf_bessel_jl(C.int(l), C.double(x))
+	}
+	return float64(y)
+}
+
+// SphBesselArr returns an array of Jl(x) where l runs from 0 to nmax inclusive
+//
+// Note that GSL has two implementations; we use the default one, not the one based
+// on Steed's algorithm.
+func SphBesselArr(lmax int, x float64) []float64 {
+	arr := make([]float64, lmax+1)
+	ret := C.gsl_sf_bessel_jl_array(C.int(lmax), C.double(x), (*C.double)(&arr[0]))
 	if ret != 0 {
 		panic(gsl.Errno(ret))
 	}
--- gsl/sf/spec/bessel_test.go
+++ gsl/sf/spec/bessel_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Bessel", func() {
+var _ = Describe("BesselJ", func() {
 	Context("value at 0", func() {
 		It("should be 1 for n=0", func() {
 			Expect(BesselJ(0, 0)).To(BeNumerically("~", 1, 1.e-13))
@@ -46,6 +46,59 @@ var _ = Describe("Bessel", func() {
 			-0.099749897617795251674, 0.11295351825659748032,
 			0.25921368809769757684}
 		arr := BesselJArr(0, 10, x)
+		It("should have length 11", func() {
+			Expect(arr).To(HaveLen(11))
+		})
+		It("should agree with the above", func() {
+			for i, j1 := range js {
+				Expect(arr[i]).To(BeNumerically("~", j1, 1.e-13))
+			}
+		})
+	})
+})
+
+var _ = Describe("SphBessel", func() {
+	Context("value at 0", func() {
+		It("should be 1 for n=0", func() {
+			Expect(SphBessel(0, 0)).To(BeNumerically("~", 1, 1.e-13))
+		})
+		It("should be 0 for n > 0", func() {
+			for i := 1; i < 25; i++ {
+				Expect(SphBessel(i, 0)).To(BeNumerically("~", 0, 1.e-13))
+			}
+		})
+	})
+
+	Context("test SphBessel at some values", func() {
+		xx := []float64{0.1, 1.0, 2, 10.0, 100}
+		j0x := []float64{0.99833416646828152307, 0.84147098480789650665,
+			0.45464871341284084770, -0.054402111088936981340,
+			-0.0050636564110975879366}
+		j5x := []float64{9.6163102329164460441e-10, 0.000092561158611258163567,
+			0.0026351697702441173490, -0.055534511621452180909,
+			-0.0092901489349075717663}
+		j7x := []float64{4.9318874757319734185e-14,
+			4.7901341987394885770e-7, 0.000056096557033489486513,
+			0.11338623065577473648, 0.0097006298438983563051}
+		It("should agree with the above", func() {
+			for i, x1 := range xx {
+				Expect(SphBessel(0, x1)).To(BeNumerically("~", j0x[i], 1.e-13))
+				Expect(SphBessel(5, x1)).To(BeNumerically("~", j5x[i], 1.e-13))
+				Expect(SphBessel(7, x1)).To(BeNumerically("~", j7x[i], 1.e-13))
+			}
+		})
+	})
+
+	Context("test SphBesselArr at some values", func() {
+		// J[0-9, 12.75]
+		x := 12.75
+		js := []float64{0.014321500755383059466, -0.075989485983702641022,
+			-0.032201379810371916177, 0.063361493901203850364,
+			0.066988082344366186965, -0.016075788716945365447,
+			-0.080857390257024933625, -0.066367040564727115896,
+			0.0027785190044047972768, 0.070071732570600178932,
+			0.10164210208119546937}
+		arr := SphBesselArr(10, x)
 		It("should have length 11", func() {
 			Expect(arr).To(HaveLen(11))
 		})
